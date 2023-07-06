91f2843cf456a8261f1652c43a629b4f9764096d
Valentine-pl <valentinpl737@gmail.com>
2023-07-06 12:51:01 +0300

    Update file

--- transformations.py
+++ transformations.py
@@ -3,5 +3,6 @@
 def my_transformation(df, target_bucket, target_key):
     # Write DataFrame to Parquet on S3
     #df.write.parquet(f's3a://{target_bucket}/{target_key}')
+    df = df.drop('product_name')
     df.coalesce(1).write.parquet(f's3a://{target_bucket}/{target_key}')
 
